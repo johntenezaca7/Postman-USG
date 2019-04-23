4d510d549f4a2d8ed14446fea6328caef41b2844
John Tenezaca <john.tenezaca7@gmail.com>
2019-04-23 13:45:20 -0400

    removed liveserver, trying something else with jenkins reports

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 var newman = require('newman');
-var liveServer = require("live-server");
-
-var params = {
-    port: 8581, // Set the server port. Defaults to 8080.
-    host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
-    root: "./newman", // Set root directory that's being served. Defaults to cwd.
-    open: true, // When false, it won't load your browser by default.
-    ignore: 'scss,my/templates', // comma-separated string for paths to ignore
-     // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
-    wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
-    mount: [['/components', './node_modules']], // Mount a directory to a route.
-    logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
-    middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
-};
 
 newman.run(
   {
     collection: 'https://www.getpostman.com/collections/cfb4d71641834fdaf493',
-    reporters: ['cli', 'html'],
+    reporters: ['cli'],
     color:'on',
     environment: 'usg.postman_env.json'
   },
@@ -29,7 +15,6 @@ newman.run(
   }
   else {
       console.log('collection run completed.');
-      liveServer.start(params);
   }
 });
 
