7afc3ffae25626cc148555cb0691d3512191fbe2
John Tenezaca <john.tenezaca7@gmail.com>
2019-04-23 12:14:41 -0400

    updated format for js file

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var newman = require('newman');
 
-newman.run({
+newman.run(
+  {
     collection: 'https://www.getpostman.com/collections/cfb4d71641834fdaf493',
     reporters: 'cli',
     color:'on',
     environment: 'usg.postman_env.json'
-}, function (err) {
-	if (err) { throw err; }
+  },
+  function (err) {
+	  if (err) { throw err; }
     console.log('collection run complete!');
-});
+  }
+);
