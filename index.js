7772e8534c197f9e7a3e3db6f0f4fe8bf0ca01d3
John Tenezaca <john.tenezaca7@gmail.com>
2019-04-23 12:11:47 -0400

    created index file to run newman as lib

---
+++ index.js
@@ -0,0 +1,11 @@
+var newman = require('newman');
+
+newman.run({
+    collection: 'https://www.getpostman.com/collections/cfb4d71641834fdaf493',
+    reporters: 'cli',
+    color:'on',
+    environment: 'usg.postman_env.json'
+}, function (err) {
+	if (err) { throw err; }
+    console.log('collection run complete!');
+});
