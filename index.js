75d2dfe6f498845aeda0736b83c556f9ae3d0445
John Tenezaca <john.tenezaca7@gmail.com>
2019-04-23 14:54:41 -0400

    changing to regular reports and cli

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ var newman = require('newman');
 newman.run(
   {
     collection: 'https://www.getpostman.com/collections/cfb4d71641834fdaf493',
-    reporters: 'htmlextra',
-    reporter: {
-      htmlextra: {
-          template: './template.hbs' // optional, the default template will be used if one is not specified
-      }
-    },
+    reporters: ['cli', 'html'],
+    color:'on',
     environment: 'usg.postman_env.json'
   },
 ).on('start', function (err, args) { // on start of run, log to console
