3e3be60a3dd6a40c5f24fdbe10e19b3b67485e1e
John Tenezaca <john.tenezaca7@gmail.com>
2019-04-23 14:19:48 -0400

    updating index file

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ var newman = require('newman');
 newman.run(
   {
     collection: 'https://www.getpostman.com/collections/cfb4d71641834fdaf493',
-    reporters: ['cli'],
+    reporters: ['cli', 'htmlextra'],
     color:'on',
     environment: 'usg.postman_env.json'
   },
 ).on('start', function (err, args) { // on start of run, log to console
-  console.log('running a collection...');
+  console.log('Running a collection...');
 }).on('done', function (err, summary) {
   if (err || summary.error) {
-      console.error('collection run encountered an error.');
+    console.error('collection run encountered an error.');
   }
   else {
-      console.log('collection run completed.');
+    console.log('collection run completed.');
   }
 });
 
