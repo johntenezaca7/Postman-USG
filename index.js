84a5b87cebe1858c01c366fb9971e9cdc12132cb
John Tenezaca <john.tenezaca7@gmail.com>
2019-04-25 10:11:48 -0400

    refactored index.js

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const newman = require('newman');
-const fs = require('fs');
-
-
 const NAME = 'USG Endpoint';
-const FILENAME = `${NAME} ${new Date().toISOString().substring(0,10)}`.split(' ').join('_');
 
 newman.run({
     collection: 'https://www.getpostman.com/collections/cfb4d71641834fdaf493',
     reporters: ['cli', 'htmlextra'],
-    color:'on',
     environment: 'usg.postman_env.json'
   })
   .on('start', 
@@ -19,7 +14,7 @@ newman.run({
   .on('done', 
     function (err, summary) {
       if (err || summary.error) {
-        console.error(`Collection run encountered an ERROR: ${ err || summary.error}.`);
+        console.error(`Collection run encountered an ERROR: ${ err || summary.error }.`);
       }
       else {
         console.log(`Collection run for ${ NAME } completed.`);
