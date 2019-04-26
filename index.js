cf539ad4d8feb33b7743b93b33ba400cba318a82
John Tenezaca <john.tenezaca7@gmail.com>
2019-04-26 16:38:19 -0400

    updated js file with more error checking

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const NAME = 'USG Endpoint';
 newman.run({
     collection: 'https://www.getpostman.com/collections/cfb4d71641834fdaf493',
     reporters: ['cli', 'htmlextra'],
-    environment: 'usg.postman_env.json'
+    environment: 'usg.postman_env.json',
+    bail: true,
+    timeoutRequest: 10000
   })
   .on('start', 
     function (err, args) { 
@@ -13,10 +15,16 @@ newman.run({
   )
   .on('done', 
     function (err, summary) {
-      if (err || summary.error) {
-        console.error(`Collection run encountered an ERROR: ${ err || summary.error }.`);
-      }
-      else {
+      const executions = summary.run.executions[0];
+      const failures = summary.run.failures[0];
+    
+      const collatedErrors = [err, summary.error, executions.requestError, failures].filter(error => error);
+
+    if ( collatedErrors.length ) {
+      collatedErrors.forEach(err => {
+        console.error(`Collection run encountered ERRORs: ${ JSON.stringify(err) }.`);
+      })
+    } else {
         console.log(`Collection run for ${ NAME } completed.`);
       }
     }
